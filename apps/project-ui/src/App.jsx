3151d421e53f5a2ddc0b0643acbe071b4ca327d7
esrabudakk <esra_b1644@outlook.com>
2023-12-01 22:41:17 +0300

    new: keycloak integration

--- apps/project-ui/src/App.jsx
+++ apps/project-ui/src/App.jsx
@@ -1,33 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Protected from "./components/Protected";
+import Public from "./components/Public";
+
+import useAuth from "./hooks/useAuth";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1> kingg </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Esra Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [isLogin, token] = useAuth();
+    console.log(token);
+    return isLogin ? <Protected token={token} /> : <Public />;
 }
 
-export default App
+export default App;
