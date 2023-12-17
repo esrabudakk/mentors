3b29057f619d4ec15f3c0ea7d7d635fae272b7dd
deryayildirimm <deryayildirimm@outlook.com.tr>
2023-12-18 02:09:16 +0300

    make some unimportant change for now

--- apps/project-ui/components/network/services/baseService.js
+++ apps/project-ui/components/network/services/baseService.js
@@ -1,24 +1,29 @@
 import axios from 'axios';
-//import { API_URL } from '../env/config';
 
 const API_URL = 'https://northwind.vercel.app/api'
-const API_URL2 = "http://157.230.84.216:3000"
+//const API_URL2 = "http://157.230.84.216:3000"
+const API_URL2 = `${import.meta.env.VITE_BASE_URL}`
+
+
+
 
 export const baseService = {
-    get: async (url) => {
-        const response = await axios.get(API_URL2 + url);
+
+
+    get: async (url, token) => {
+        const response = await axios.get(API_URL2 + url , {token});
         return response.data;
     },
      getTest: async (url) => {
-        const response = await axios.get(API_URL + url);
+        const response = await axios.get(API_URL + url, {token});
         return response.data;
     },
-    delete: async (url) => {
-        const response = await axios.delete(API_URL2 + url);
+    delete: async (url,token) => {
+        const response = await axios.delete(API_URL2 + url, {token});
         return response;
     },
-    post: async (url, data) => {
-        const response = await axios.post(API_URL2 + url, data);
+    post: async (url, data, token) => {
+        const response = await axios.post(API_URL2 + url, data, {token});
         return response;
     },
 };
