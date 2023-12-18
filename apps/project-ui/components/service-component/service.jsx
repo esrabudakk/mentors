a503099692810236524512a358e432369721480d
deryayildirimm <deryayildirimm@outlook.com.tr>
2023-12-18 06:37:39 +0300

    design detail page

--- apps/project-ui/components/service-component/service.jsx
+++ apps/project-ui/components/service-component/service.jsx
@@ -136,7 +136,8 @@ const ServicePage = ({ id, title, breif, descr, imageSrc }) => {
               <Card maxW="ml" width="300px" height="480px" key={i}>
                 <CardBody height="500px">
                   <Image
-                    src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                    src= {`https://picsum.photos/id/${e.id}/300/250`}
+                    // "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
                     alt="Green double couch with wooden legs"
                     borderRadius="lg"
                   />
